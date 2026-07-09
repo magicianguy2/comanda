2d12f6bc2768ebd6f2abab0e313a836276cabeda
Magicianguy2 <esseaquiehpragames@gmail.com>
2026-07-09 20:31:04 -0300

    Update GitHub Pages web build in docs

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "547868672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3986175108" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
